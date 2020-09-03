ad023aa4a522f20231d79985b12a3f1f6c19ad85
habinezajanvier <habinezajanvier688@gmail.com>
2020-09-03 16:17:26 +0200

    fix(mantain): connect front-end with backend

--- UI/scripts/account.js
+++ UI/scripts/account.js
@@ -1,21 +1,7 @@
 //Loging out if someone clicked on logout
-element('.logout').addEventListener('click', () => {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      window.location = '../login/';
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    profileNavigation(user);
-    accountSection(user);
-  }
+element(".logout").addEventListener("click", () => {
+  localStorage.removeItem("auth-token");
+  window.location = "../login/";
 });
 
 function displayImage(photo) {
@@ -25,7 +11,7 @@ function displayImage(photo) {
 }
 
 function profileData(user) {
-  const { displayName, email, photoURL, phoneNumber, bio } = user;
+  const { fullNames, email, photoURL, phoneNumber, bio } = user;
   return `
           <div class="account-wrapper">
             <div class="profile-reveiw">
@@ -33,11 +19,11 @@ function profileData(user) {
                 ${displayImage(photoURL)}
                 <input type="file" name="profile-photo" id="profile-photo" />
               </div>
-              <b>${displayName}</b>
+              <b>${fullNames}</b>
             </div>
             <div class="profile-data">
               <p>${email}</p>
-              <p>${phoneNumber || ''}</p>
+              <p>${phoneNumber || ""}</p>
               <div class="edit-profile" >
                 <i class="fas fa-user-plus"></i>Edit profile
               </div>
@@ -45,35 +31,35 @@ function profileData(user) {
             <div class="user-bio">
               <b>Bio</b>
               <p>
-                ${bio || ''}
+                ${bio || ""}
               </p>
             </div>-->
           </div>
   `;
 }
 function accountSection(user) {
-  element('#user-account').innerHTML = profileData(user);
-  element('.edit-profile').addEventListener('click', () => {
+  element("#user-account").innerHTML = profileData(user);
+  element(".edit-profile").addEventListener("click", () => {
     profileEdit(user);
   });
 }
 
 function profileNavigation(user) {
-  const { photoURL, displayName, email } = user;
-  const profile = element('#profile-nav');
-  const profilePreview = element('.profile-preview');
+  const { photoURL, fullNames, email } = user;
+  const profile = element("#profile-nav");
+  const profilePreview = element(".profile-preview");
   profile.innerHTML = `${displayImage(photoURL)}<span>${
-    displayName || ''
+    fullNames || ""
   }</span>`;
   profilePreview.innerHTML = `${displayImage(photoURL)}
   <div class="profile-names">
     <em>${email}</em><br />
-    <b>${displayName || ''}</b>
+    <b>${fullNames || ""}</b>
   </div>`;
 }
 
 function profileEditForm(user) {
-  const { displayName, phoneNumber } = user;
+  const { fullNames, phoneNumber } = user;
   return `
           <form autocomplete="off" id="edit-profile">
           <h3>EDIT YOUR PROFILE</h3>
@@ -81,7 +67,7 @@ function profileEditForm(user) {
             <label for="Names">
               Full Names
               <input type="text" id="edit-name" value='${
-                displayName || ''
+                fullNames || ""
               }' required/>
             </label>
             <!--
@@ -89,6 +75,7 @@ function profileEditForm(user) {
               Phone Number
               <input type="text" value='${phoneNumber}' id="edit-phones"/>
             </label>
+            -->
             <label for="bio">
               Your Bio
               <textarea
@@ -96,48 +83,76 @@ function profileEditForm(user) {
                 rows="10"
               ></textarea>
             </label>
-            -->
+           
             <button>Save Changes</button>
           </form>
 `;
 }
 function profileEdit(user) {
-  element('#user-account').innerHTML = profileEditForm(user);
-  element('.close').addEventListener('click', () => {
+  element("#user-account").innerHTML = profileEditForm(user);
+  element(".close").addEventListener("click", () => {
     accountSection(user);
     swithDisplay(
-      '.account-wrapper',
-      '#adding-article',
-      '#dashboard',
-      '#articles',
-      '#messages'
+      ".account-wrapper",
+      "#adding-article",
+      "#dashboard",
+      "#articles",
+      "#messages"
     );
   });
-  element('#edit-profile').addEventListener('submit', (e) => {
+  element("#edit-profile").addEventListener("submit", (e) => {
     e.preventDefault();
     editProfile(user);
   });
 }
 
 function editProfile(user) {
-  displayName = element('#edit-name').value;
+  fullNames = element("#edit-name").value;
   // phoneNumber = element('#edit-phones').value;
-  // bio = element('#edit-bio').value;
-  user
-    .updateProfile({
-      displayName,
-    })
-    .then(() => {
+  bio = element("#edit-bio").value;
+  fetch("https://my-brand-api.herokuapp.com/users/profile", {
+    method: "PUT",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "auth-token": localStorage.getItem("auth-token"),
+    },
+    body: JSON.stringify({
+      fullNames,
+      // bio,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
       accountSection(user);
       swithDisplay(
-        '.account-wrapper',
-        '#adding-article',
-        '#dashboard',
-        '#articles',
-        '#messages'
+        ".account-wrapper",
+        "#adding-article",
+        "#dashboard",
+        "#articles",
+        "#messages"
       );
     })
     .catch((error) => {
       console.log(error);
     });
 }
+
+window.onload = () => {
+  const token = localStorage.getItem("auth-token");
+  if (!token) {
+    window.location = "../login/";
+  } else {
+    const parseJwt = (token) => {
+      try {
+        return JSON.parse(atob(token.split(".")[1]));
+      } catch (e) {
+        return null;
+      }
+    };
+    const user = parseJwt(token);
+    accountSection(user);
+    profileNavigation(user);
+  }
+};
--- UI/scripts/blog.js
+++ UI/scripts/blog.js
@@ -1,25 +1,31 @@
 const updatePreviewSection = () => {
-  postRef.on('value', (snap) => {
-    const articleIds = Object.keys(snap.val());
-    articleIds.forEach((a) => {
-      selector('.all-article-list').append(
-        preveiwArticle(
-          a,
-          snap.val()[a].imageUrl,
-          snap.val()[a].title,
-          snap.val()[a].lastEdit,
-          snap.val()[a].views.length
-        )
-      );
-    });
-    document.querySelectorAll('.single-article-preview').forEach((element) => {
-      element.addEventListener('click', singleArticleRedirection);
-    });
-  });
+  let status;
+  fetch("https://my-brand-api.herokuapp.com/articles")
+    .then((res) => {
+      status = res.status;
+      return res.json();
+    })
+    .then((data) => {
+      if (status === 200) {
+        data.forEach((a) => {
+          selector(".all-article-list").append(
+            preveiwArticle(a._id, a.imageUrl, a.title, a.date, a.views)
+          );
+        });
+        document
+          .querySelectorAll(".single-article-preview")
+          .forEach((element) => {
+            element.addEventListener("click", singleArticleRedirection);
+          });
+      } else {
+        selector(".all-article-list").innerHTML = data.error || data.msg;
+      }
+    })
+    .catch((error) => console.log(error));
 };
 
 const updatePopularStory = () => {
-  postRef.on('value', (snapshot) => {
+  postRef.on("value", (snapshot) => {
     const allViews = [];
     const viewedArticle = [];
     const ids = Object.keys(snapshot.val());
@@ -36,14 +42,14 @@ const updatePopularStory = () => {
           (article) => article.views.length === allViews[1]
         );
         if (allViews.length >= 3) {
-          selector('#pop-story-2').innerHTML = '';
-          selector('#pop-story-2').innerHTML = popularArticle(
+          selector("#pop-story-2").innerHTML = "";
+          selector("#pop-story-2").innerHTML = popularArticle(
             secondArticle[0].imageUrl,
             secondArticle[0]
           );
         }
-        selector('#pop-story-1').innerHTML = '';
-        selector('#pop-story-1').innerHTML = popularArticle(
+        selector("#pop-story-1").innerHTML = "";
+        selector("#pop-story-1").innerHTML = popularArticle(
           firstArticle[0].imageUrl,
           firstArticle[0]
         );
@@ -53,7 +59,7 @@ const updatePopularStory = () => {
 };
 
 const updateLikedSection = () => {
-  postRef.on('value', (snap) => {
+  postRef.on("value", (snap) => {
     let likes = [];
     let likedArticle = [];
     const articleIds = Object.keys(snap.val());
@@ -66,8 +72,8 @@ const updateLikedSection = () => {
           (a) => a.likes === Math.max(...likes)
         );
 
-        selector('.w-100').innerHTML = '';
-        selector('.w-100').innerHTML = mostLikedArticle(
+        selector(".w-100").innerHTML = "";
+        selector(".w-100").innerHTML = mostLikedArticle(
           mostLiked[0].imageUrl,
           mostLiked[0]
         );
